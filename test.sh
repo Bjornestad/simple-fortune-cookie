3a3410b49cbdf6f0abceaa64b645b85eb44937b0
DenFlyvendeGris <112903007+DenFlyvendeGris@users.noreply.github.com>
2024-08-16 16:57:02 +0200

    Update test.sh
    I have updated the if statement, since the syntax was wrong. I have no idea if it works now, but let's hope

--- test.sh
+++ test.sh
@@ -1,8 +1,11 @@
-port=$(kubectl get service | grep frontend | tr -s ' ' | cut -d ' ' -f 5 | cut -c5-10)
+port=$(kubectl get service | grep frontend | tr -s ' ' | cut -d ' ' -f 5 | cut -c4-10)
 echo port is $port
 ip=$(kubectl get nodes -o wide | head -n 2 | awk '{print $7}' | sed -n 2p)
 echo ip is $ip
-if [ $(curl $port$ip/healthz) = "healthy" ]; then
-    exit 1
+if [ $(curl http://$ip$port/healthz) = "healthy" ]; then
+    echo "This is healthy"
+    exit 0
+else
+    echo "This is not healthy"
+exit 1
 fi
-exit 0
