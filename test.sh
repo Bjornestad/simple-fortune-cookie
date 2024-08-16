a5522aecb0c4927229e2f191fbbec24bf1510ed8
Yalcin Acili <yalcinacili@gmail.com>
2024-08-16 17:38:32 +0000

    did a whoopsie with the echo kconfig commands

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
-port=$(run: echo "${{ secrets.KUBECONFIG_TEST }}" > kubeconfig && kubectl --kubeconfig kubeconfig get service | grep frontend | tr -s ' ' | cut -d ' ' -f 5 | cut -c4-10)
+port=$(echo "${{ secrets.KUBECONFIG_TEST }}" > kubeconfig && kubectl --kubeconfig kubeconfig get service | grep frontend | tr -s ' ' | cut -d ' ' -f 5 | cut -c4-10)
 echo port is $port
-ip=$(run: echo "${{ secrets.KUBECONFIG_TEST }}" > kubeconfig && kubectl --kubeconfig kubeconfig get nodes -o wide | head -n 2 | awk '{print $7}' | sed -n 2p)
+ip=$(echo "${{ secrets.KUBECONFIG_TEST }}" > kubeconfig && kubectl --kubeconfig kubeconfig get nodes -o wide | head -n 2 | awk '{print $7}' | sed -n 2p)
 echo ip is $ip
 if [ $(curl http://$ip$port/healthz) = "healthy" ]; then
     echo "This is healthy"
