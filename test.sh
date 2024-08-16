00f5f1e66b15a164952930dab8a343e18f2d44ee
Mads <msigs22@student.sdu.dk>
2024-08-16 17:53:17 +0000

    fixed test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-port=$(echo "${{ secrets.KUBECONFIG_TEST }}" > kubeconfig && kubectl --kubeconfig kubeconfig get service | grep frontend | tr -s ' ' | cut -d ' ' -f 5 | cut -c4-10)
+port=$(echo "${{ secrets.KUBECONFIG_TEST }}" > kubeconfig && kubectl --kubeconfig kubeconfig get service | grep frontend | tr -s ' ' | cut -d ' ' -f 5 | cut -c5-10)
 echo port is $port
 ip=$(echo "${{ secrets.KUBECONFIG_TEST }}" > kubeconfig && kubectl --kubeconfig kubeconfig get nodes -o wide | head -n 2 | awk '{print $7}' | sed -n 2p)
 echo ip is $ip
